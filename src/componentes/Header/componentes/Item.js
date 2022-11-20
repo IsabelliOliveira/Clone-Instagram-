50c2b34ecaf1acce0e1a0208ddb1811b9ec4f853
Isabelli Oliveira <104120168+IsabelliOliveira@users.noreply.github.com>
2022-11-20 05:07:25 -0300

    Commit

--- src/componentes/Header/componentes/Item.js
+++
@@ -1,8 +0,0 @@
-export function Item (props) {
-    return(
-        <div className="item">
-            <span className="iconeItem">{props.icone}</span>
-            <span>{props.nome}</span>&nbsp;
-        </div>
-    )
-}
